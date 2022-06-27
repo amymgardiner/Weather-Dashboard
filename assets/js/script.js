8ea17a7a5a5b4966966021b5326a267a24c411f3
Amy Gardiner <amymgardiner@gmail.com>
2022-06-27 15:32:56 -0500

    got searches saved into local storage and display as buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,26 +2,18 @@ var apiKey = "66bf34535fb3ce4cc5102bab886f4c2b"
 searchBtn = document.querySelector(".search-button")
 var cityTitle = document.querySelector("#city-name")
 var cityButtons = document.querySelector("#city-buttons")
+var currentWeatherContainer = document.querySelector("#current-city")
+var searchValue = document.getElementById('search-input')
+var recentSearch = JSON.parse(localStorage.getItem("recentsearch"))
 
-
-// function to get city's zip code from user - then that zip code is saved and used to call the next function
+// function to get city's zip code from user
+// the zip code is used to get the city's name for the HTML and used to get the latitude and longitude
+// the lat and lon are used to call the next function
 var getZipCode = function(event) {
     event.preventDefault()
     var zipCode = document.querySelector(".form-control").value.trim()
-    getLatLon(zipCode)
-}
-
-var pastSearchHandler = function(event){
-    getWeather(storedSearches.lat)
-}
-
-
-
-// the zip code from the precious code is used to get the city's name for the HTML and used to get the latitude and longitude
-// the lat and lon are used to call the next function
-var getLatLon = function(zipCode) {
     var zipApi = `http://api.openweathermap.org/geo/1.0/zip?zip=${zipCode}&appid=${apiKey}`
-
+   
     fetch(zipApi)
     .then(function(response) {
         if(response.ok){
@@ -33,19 +25,14 @@ var getLatLon = function(zipCode) {
                 getWeather(lat, lon)
                 fivedayForecast(lat, lon)
 
-                var cityDiv = document.querySelector("#current-city")
-                cityDiv.classList = "border border-dark div-color"
+                currentWeatherContainer.classList = "border border-dark div-color"
 
                 // get city name from api and display on screen in current weather section
                 var cityName = data.name
-                cityTitle.innerHTML = cityName;
+                cityTitle.innerHTML = cityName
 
-                // localStorage.setItem("recentSearch", cityName)
-                localStorage.setItem("recentSearch", JSON.stringify(data))
-
-                var storedSearches = JSON.parse(localStorage.getItem("recentSearch"));
-                console.log(storedSearches)
-                cityButtons.innerHTML += `<button type="button" class="btn cities">${storedSearches.name}</button>`
+                localStorage.setItem("recentsearch", JSON.stringify(data))
+                cityButtons.innerHTML += `<button type="button" class="btn cities">${data.name} + ${data.zip}</button>`
             })
         } else {
             displayError()
@@ -63,6 +50,7 @@ var todayDate = function(timeZone) {
 
 // the weather app is finally called to get the rest of the html weather information using the lat and lon
 var getWeather = function(lat, lon) {
+    searchValue.value = ""
     var weatherApi = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&appid=${apiKey}`
 
     fetch(weatherApi)
@@ -114,35 +102,35 @@ var fivedayForecast = function (lat, lon) {
                 document.getElementById("forecast").textContent = "Five Day Forecast"
                 document.getElementById("five-day-forecast").classList = "border border-dark div-colortwo"
 
-                    document.getElementById("dayone").innerHTML = luxon.DateTime.fromMillis((data.daily[0].dt * 1000)).toFormat('MM-dd-yyyy')
-                    document.getElementById("daytwo").innerHTML = luxon.DateTime.fromMillis((data.daily[1].dt * 1000)).toFormat('MM-dd-yyyy')
-                    document.getElementById("daythree").innerHTML = luxon.DateTime.fromMillis((data.daily[2].dt * 1000)).toFormat('MM-dd-yyyy')
-                    document.getElementById("dayfour").innerHTML = luxon.DateTime.fromMillis((data.daily[3].dt * 1000)).toFormat('MM-dd-yyyy')
-                    document.getElementById("dayfive").innerHTML = luxon.DateTime.fromMillis((data.daily[4].dt * 1000)).toFormat('MM-dd-yyyy')
+                    document.getElementById("dayone").innerHTML = luxon.DateTime.fromMillis((data.daily[1].dt * 1000)).toFormat('MM-dd-yyyy')
+                    document.getElementById("daytwo").innerHTML = luxon.DateTime.fromMillis((data.daily[2].dt * 1000)).toFormat('MM-dd-yyyy')
+                    document.getElementById("daythree").innerHTML = luxon.DateTime.fromMillis((data.daily[3].dt * 1000)).toFormat('MM-dd-yyyy')
+                    document.getElementById("dayfour").innerHTML = luxon.DateTime.fromMillis((data.daily[4].dt * 1000)).toFormat('MM-dd-yyyy')
+                    document.getElementById("dayfive").innerHTML = luxon.DateTime.fromMillis((data.daily[5].dt * 1000)).toFormat('MM-dd-yyyy')
 
-                    document.getElementById("dayone-icon").innerHTML = `<img src="http://openweathermap.org/img/wn/${data.daily[0].weather[0].icon}.png">`
-                    document.getElementById("daytwo-icon").innerHTML = `<img src="http://openweathermap.org/img/wn/${data.daily[1].weather[0].icon}.png">`
-                    document.getElementById("daythree-icon").innerHTML = `<img src="http://openweathermap.org/img/wn/${data.daily[2].weather[0].icon}.png">`
-                    document.getElementById("dayfour-icon").innerHTML = `<img src="http://openweathermap.org/img/wn/${data.daily[3].weather[0].icon}.png">`
-                    document.getElementById("dayfive-icon").innerHTML = `<img src="http://openweathermap.org/img/wn/${data.daily[4].weather[0].icon}.png">`
+                    document.getElementById("dayone-icon").innerHTML = `<img src="http://openweathermap.org/img/wn/${data.daily[1].weather[0].icon}.png">`
+                    document.getElementById("daytwo-icon").innerHTML = `<img src="http://openweathermap.org/img/wn/${data.daily[2].weather[0].icon}.png">`
+                    document.getElementById("daythree-icon").innerHTML = `<img src="http://openweathermap.org/img/wn/${data.daily[3].weather[0].icon}.png">`
+                    document.getElementById("dayfour-icon").innerHTML = `<img src="http://openweathermap.org/img/wn/${data.daily[4].weather[0].icon}.png">`
+                    document.getElementById("dayfive-icon").innerHTML = `<img src="http://openweathermap.org/img/wn/${data.daily[5].weather[0].icon}.png">`
 
-                    document.getElementById("dayone-temp").innerHTML = "Temp: " + data.daily[0].temp.day
-                    document.getElementById("daytwo-temp").innerHTML = "Temp: " + data.daily[1].temp.day
-                    document.getElementById("daythree-temp").innerHTML = "Temp: " + data.daily[2].temp.day
-                    document.getElementById("dayfour-temp").innerHTML = "Temp: " + data.daily[3].temp.day
-                    document.getElementById("dayfive-temp").innerHTML = "Temp: " + data.daily[4].temp.day
+                    document.getElementById("dayone-temp").innerHTML = "Temp: " + data.daily[1].temp.day
+                    document.getElementById("daytwo-temp").innerHTML = "Temp: " + data.daily[2].temp.day
+                    document.getElementById("daythree-temp").innerHTML = "Temp: " + data.daily[3].temp.day
+                    document.getElementById("dayfour-temp").innerHTML = "Temp: " + data.daily[4].temp.day
+                    document.getElementById("dayfive-temp").innerHTML = "Temp: " + data.daily[5].temp.day
 
-                    document.getElementById("dayone-wind").innerHTML = "Wind: " + data.daily[0].wind_speed
-                    document.getElementById("daytwo-wind").innerHTML = "Wind: " + data.daily[1].wind_speed
-                    document.getElementById("daythree-wind").innerHTML = "Wind: " + data.daily[2].wind_speed
-                    document.getElementById("dayfour-wind").innerHTML = "Wind: " + data.daily[3].wind_speed
-                    document.getElementById("dayfive-wind").innerHTML = "Wind: " + data.daily[4].wind_speed
+                    document.getElementById("dayone-wind").innerHTML = "Wind: " + data.daily[1].wind_speed
+                    document.getElementById("daytwo-wind").innerHTML = "Wind: " + data.daily[2].wind_speed
+                    document.getElementById("daythree-wind").innerHTML = "Wind: " + data.daily[3].wind_speed
+                    document.getElementById("dayfour-wind").innerHTML = "Wind: " + data.daily[4].wind_speed
+                    document.getElementById("dayfive-wind").innerHTML = "Wind: " + data.daily[5].wind_speed
 
-                    document.getElementById("dayone-humidity").innerHTML = "Humidity: " + data.daily[0].humidity
-                    document.getElementById("daytwo-humidity").innerHTML = "Humidity: " + data.daily[1].humidity
-                    document.getElementById("daythree-humidity").innerHTML = "Humidity: " + data.daily[2].humidity
-                    document.getElementById("dayfour-humidity").innerHTML = "Humidity: " + data.daily[3].humidity
-                    document.getElementById("dayfive-humidity").innerHTML = "Humidity: " + data.daily[4].humidity
+                    document.getElementById("dayone-humidity").innerHTML = "Humidity: " + data.daily[1].humidity
+                    document.getElementById("daytwo-humidity").innerHTML = "Humidity: " + data.daily[2].humidity
+                    document.getElementById("daythree-humidity").innerHTML = "Humidity: " + data.daily[3].humidity
+                    document.getElementById("dayfour-humidity").innerHTML = "Humidity: " + data.daily[4].humidity
+                    document.getElementById("dayfive-humidity").innerHTML = "Humidity: " + data.daily[5].humidity
             })
         }
     })
@@ -165,4 +153,3 @@ closeButton.addEventListener("click", function closeButtonHandler() {
 
 // all functions run after user enters in a zip code and clicks the search button
 searchBtn.addEventListener("click", getZipCode)
-cityButtons.addEventListener("click", pastSearchHandler);
